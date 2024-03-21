c8a12e295695898f591c69ae530ae1be8f7e35cf
Vikram Saini <vikramjit.personal@gmail.com>
2024-03-21 15:04:17 +0530

    Making useless changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { usePosts, PostProvider } from "./PostContext";
 
